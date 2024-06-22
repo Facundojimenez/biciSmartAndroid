24ca529d1eb918b18d97905f948889289847f8c0
Fran <fgarcete@alumno.unlam.edu.ar>
2024-06-22 19:10:10 -0300

    value check

--- app/src/main/java/com/example/bicismart/TrainingSetupFragment.java
+++ app/src/main/java/com/example/bicismart/TrainingSetupFragment.java
@@ -3,6 +3,7 @@ package com.example.bicismart;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -22,8 +23,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class TrainingSetupFragment extends Fragment
-{
+public class TrainingSetupFragment extends Fragment {
 
     RadioButton btnTime, btnMeters;
     private TextView tvTrainningParameter;
@@ -44,13 +44,11 @@ public class TrainingSetupFragment extends Fragment
     //private boolean enableSensor = true;
     private boolean enableDinMusic = true;
 
-    public TrainingSetupFragment()
-    {
+    public TrainingSetupFragment() {
         // Required empty public constructor
     }
 
-    public static TrainingSetupFragment newInstance(String address)
-    {
+    public static TrainingSetupFragment newInstance(String address) {
         TrainingSetupFragment fragment = new TrainingSetupFragment();
         Bundle bundle = new Bundle();
         bundle.putString("Direccion_Bluetooth", address);
@@ -59,19 +57,15 @@ public class TrainingSetupFragment extends Fragment
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null)
-        {
+        if (getArguments() != null) {
             address = getArguments().getString("Direccion_Bluetooth");
         }
 
-        getParentFragmentManager().setFragmentResultListener("datos", this, new FragmentResultListener()
-        {
+        getParentFragmentManager().setFragmentResultListener("datos", this, new FragmentResultListener() {
             @Override
-            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result)
-            {
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
                 enableBuzzer = result.getBoolean("Buzzer");
 
                 enableDinMusic = result.getBoolean("Musica_Dinamica");
@@ -80,15 +74,13 @@ public class TrainingSetupFragment extends Fragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.tr_fragment_training_setup, container, false);
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
-    {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         btnTime = view.findViewById(R.id.btn_tiempo);
         btnMeters = view.findViewById(R.id.btn_metros);
@@ -102,14 +94,13 @@ public class TrainingSetupFragment extends Fragment
         // esto es para cambiar el color del 1er item seleccionado
         spItensity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                TextView ch = ((TextView)spItensity.getChildAt(0));
+                TextView ch = ((TextView) spItensity.getChildAt(0));
                 if (ch != null) {
                     ch.setTextColor(Color.parseColor("#2E7D32"));
                 }
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
-                return;
             }
         });
 
@@ -117,18 +108,15 @@ public class TrainingSetupFragment extends Fragment
         address = args.getString("Direccion_Bluetooth", address);
         showToast("Address: " + address);
 
-        btnTime.setOnClickListener(new View.OnClickListener()
-        {
+        btnTime.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 tvTrainningParameter.setText("Ingresar Tiempo (en Minutos)");
             }
         });
 
-        btnMeters.setOnClickListener(new View.OnClickListener()
-        {
+        btnMeters.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
@@ -136,32 +124,43 @@ public class TrainingSetupFragment extends Fragment
             }
         });
 
-        btnStart.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                String str = etTrainningParameter.getText().toString();
-                if(str.isEmpty())
-                    showToast("Ingresar Parametros");
-                else
-                {
+        btnStart.setOnClickListener(v -> {
+            String str = etTrainningParameter.getText().toString();
+            if (str.isEmpty())
+                showToast("Ingresar Parametros");
+            else {
+                int valor = Integer.parseInt(str);
+                if (valor > 1 && valor < 100000) {
                     Intent i = new Intent(getActivity(), TrainningActivity.class);
                     i.putExtra("Direccion_Bluethoot", address);
-                    i.putExtra("Duracion", Integer.parseInt(str));
+                    i.putExtra("Duracion", valor);
                     i.putExtra("Por Tiempo", btnTime.isChecked());
                     i.putExtra("Intensidad", spItensity.getSelectedItem().toString());
                     i.putExtra("Buzzer", enableBuzzer);
-
                     i.putExtra("Musica Dinamica", enableDinMusic);
                     startActivity(i);
+                } else if (valor > 100000) {
+                    String youtubeVideoLink = "https://www.youtube.com/watch?v=BGljR9vf3Os";
+
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(youtubeVideoLink));
+                    browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                    try {
+                        startActivity(browserIntent);
+                    }catch (Exception e){
+                        showToast("Esto no era un eater egg...");
+                    }
+
+                } else if (btnTime.isChecked()) {
+                    showToast("Tiempo de entrenamiento invalido");
+                } else if (btnMeters.isChecked()) {
+                    showToast("Metros de entrenamiento invalido");
                 }
             }
         });
     }
 
-    private void showToast(String message)
-    {
+    private void showToast(String message) {
         Toast.makeText(requireActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
